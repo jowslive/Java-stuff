7980caf16bf87f2abf33e8a5d1813d4d85b7bd0f
Jows <joao.brufatto@gmail.com>
2018-06-17 18:10:33 -0300

    Stuff

---
+++ Files/Guess.java
@@ -0,0 +1,20 @@
+﻿package guess;
+
+public class Guess {
+
+    public static void main(String[] args)throws java.io.IOException {
+        char ch,answer='E';
+        System.out.println("Pense em uma letra entre A e E.");
+        System.out.println("Digite a letra que você pensou:");
+        ch=(char)System.in.read();
+        if(ch==answer)
+            
+        {
+            System.out.println("Voce acertou!");
+        }
+        else {
+            System.out.println("Voce errou!");
+        }
+    }
+    
+}
