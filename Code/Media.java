93400ede5c705a8f91e304055c1b999b71ec3322
Jows <joao.brufatto@gmail.com>
2018-06-19 11:49:30 -0300

    Stuff

---
+++ Code/Media.java
@@ -0,0 +1,14 @@
+﻿
+package Media;
+
+public class Media {
+
+    public static void main(String[] args) {
+        double np1,np2,media;
+        np1=6;
+        np2=8.3;
+        media=(np1+np2)/2;
+        System.out.println("Média: "+ media);
+    }
+    
+}
