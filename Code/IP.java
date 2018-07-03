fe8627d644f3c4cf122aaeb583dd1c061937a6f1
Jows <joao.brufatto@gmail.com>
2018-07-03 00:10:35 -0300

    Stuff

---
+++ Code/IP.java
@@ -0,0 +1,9 @@
+import java.net.InetAddress;
+ 
+public class IPAddress
+{
+   public static void main(String args[]) throws Exception
+   {
+      System.out.println(InetAddress.getLocalHost());
+   }
+}
