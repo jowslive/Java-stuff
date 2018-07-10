b907e5266b88959eccbe62d677d8827ab5879948
Jows <joao.brufatto@gmail.com>
2018-07-10 10:08:05 -0300

    Stuff

---
+++ Code/CompareEquals.java
@@ -0,0 +1,13 @@
+public class CompareStrings {
+
+    public static void main(String[] args) {
+
+        String style = new String("Java");
+        String style2 = new String("Java");
+
+        if(style.equals(style2))
+            System.out.println("Igual");
+        else
+            System.out.println("Nao e igual");
+    }
+}
